423f781695b5cc4ceba92f5731ec30c27ee7191f
sudhagar-raman <sudhagar.raman@gmail.com>
2018-04-20 00:04:07 +0530

    filters

--- public/ngapp/controllers/HomeController.js
+++ public/ngapp/controllers/HomeController.js
@@ -15,6 +15,8 @@
 			vm.title = 'Home page';
 			vm.getServerList();
 			vm.filterRam();
+			vm.fliterHdd();
+			vm.filterLocation();
 		}
 
 		// Get all servers
@@ -29,12 +31,21 @@
 
 		// Filter data
 		vm.filterRam = function(){
-			vm.optionList = ["4GB","8GB","16GB","32GB","64GB","128GB","252GB"];
+			vm.optionsList = ["2GB", "4GB","8GB","12GB","16GB","24GB","32GB","48GB", "64GB", "96GB"];
 			vm.selected = {
 			    list: ['option1']
 			  };
 		}
 
+		vm.fliterHdd = function(){
+			vm.hdd = ["SAS", "SATA", "SSD"]
+		}
+
+		vm.filterLocation = function(){
+			vm.locations = ["AmsterdamAMS-01", "Washington D.C.WDC-01", "San FranciscoSFO-12",
+							"SingaporeSIN-12", "DallasDAL-10", "FrankfurtFRA-10", "Hong KongHKG-01"
+							]
+		}
 
 
 		vm.init();
