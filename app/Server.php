0be1aaaf5675331667dfd0db821d0c2fcc516967
sudhagar-raman <sudhagar.raman@gmail.com>
2018-04-22 16:56:33 +0530

    filter implementation

--- app/Server.php
+++ app/Server.php
@@ -6,5 +6,41 @@ use Illuminate\Database\Eloquent\Model;
 
 class Server extends Model
 {
+	
     protected $fillable = array('id', 'model', 'ram', 'hdd', 'location', 'price');
+
+    /**
+      Get all server 
+      @return Array of object
+    */
+    public static function getAllServers(){
+    	return Server::orderBy('id', 'asc')->take(20)->get();
+    }
+
+    /**
+      Get server list based on the filter parameters
+      $params - array
+      @return Array of object
+    */
+    public static function getServerByFilters($params){
+      $hdd = $params['hdd'] ?? null;
+      $ram = $params['ram'] ?? array();
+      $location = $params['location'] ?? null;
+
+      $query = Server::orderBy('id', 'asc');
+
+      foreach($ram as $key => $value){
+         $query->orWhere('ram', 'like', $value.'%');
+      }
+
+      if($hdd){
+        $query->where('hdd', 'like', '%' . $params['hdd']);
+      }
+      
+      if($location){
+        $query->where('location', $params['location']);
+      }
+  
+      return $query->get();
+    }
 }
